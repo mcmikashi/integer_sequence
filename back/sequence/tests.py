8098a61d6200d00193ba1043cfc4fbea22d2fa91
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-08-19 12:04:09 -0300

    Add test for lucas sequence API

--- back/sequence/tests.py
+++ back/sequence/tests.py
@@ -101,3 +101,33 @@ class TestLucasNumbers(TestCase):
             lucas("aaaa")
             lucas(-100)
             lucas(4000)
+
+class TestLucasAPI(APITestCase):
+
+    def test_valid_data_0(self):
+        """Make sure that the api return the good value"""
+        url = reverse("sequence:lucas",kwargs={'index':0})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data,{'result':2})
+    
+    def test_valid_data_1(self):
+        """Make sure that the api return the good value"""
+        url = reverse("sequence:lucas",kwargs={'index':32})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data,{'result':4870847})
+    
+    def test_invalid_data_0(self):
+        """Make sure that the api return 404 code status and the good value"""
+        url = reverse("sequence:lucas",kwargs={'index':4000})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        self.assertIn("You send an invalid index.",response.data["detail"])
+
+    def test_invalid_data_1(self):
+        """Make sure that the api return 404 code status and the good value"""
+        url = reverse("sequence:lucas",kwargs={'index':1001})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        self.assertIn("You send an invalid index.",response.data["detail"])
