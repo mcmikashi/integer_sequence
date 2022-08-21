7c893f525ad710beed64f4a769b44ef0995bc025
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-08-21 14:38:47 -0300

    Add frontend app

---
+++ front/.routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "extensions": [
+    "html",
+    "svelte",
+    "md",
+    "svx"
+  ],
+  "started": "2022-08-21T17:37:27.553Z"
+}
