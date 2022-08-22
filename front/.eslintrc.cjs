acf7c474465dffbca131c292a8b1203741ecd333
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-08-22 11:07:39 -0300

    Add lint tools for the frontend

---
+++ front/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+        "jest": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:svelte/recommended",
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+}
