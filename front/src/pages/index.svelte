595dfd82f835bb8e34c993b87d9f4df42932cf2a
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-08-31 12:27:39 -0300

    Update the index page with the facts component

--- front/src/pages/index.svelte
+++ front/src/pages/index.svelte
@@ -1,11 +1,45 @@
-<script></script>
+<script>
+  import Facts from "../lib/Facts/Facts.svelte";
+</script>
 
-<h1>Routify Starter Template</h1>
-
-<p>
-    To see an example app that shows off a lot of Routify's features, go to <a href="/fibonacci">/example</a>
+<h3>It's a small app around numbers</h3>
+<p>What is a numbers ?</p>
+<blockquote>
+  A number is a mathematical object used to count, measure, and label. The
+  original examples are the natural numbers 1, 2, 3, 4, and so forth. Numbers
+  can be represented in language with number words. More universally, individual
+  numbers can be represented by symbols, called numerals; for example, "5" is a
+  numeral that represents the number five.
+  <p class="source-blockquote">
+    source : 
+    <cite>
+        <a href="https://en.wikipedia.org/wiki/Number" target="_blank" rel="noopener noreferrer">
+        https://en.wikipedia.org/wiki/Number
+        </a>
+    </cite>
+  </p>
+</blockquote>
+<p>Now let's see four random facts about numbers.</p>
+<Facts />
+<p class="source-blockquote">
+  source : 
+    <cite>
+        <a href="http://numbersapi.com/" target="_blank" rel="noopener noreferrer">
+            http://numbersapi.com/
+        </a>
+    </cite>
 </p>
 
-<p>
-    This template is ready to be used in production! just delete the example app at: src/pages/example
-</p>
+<style>
+  p {
+    margin: 25px 0px;
+  }
+  blockquote {
+    font-size: larger;
+    margin: 15px 0px;
+  }
+  .source-blockquote {
+    text-align: right;
+    margin: 10px 0px;
+  }
+</style>
